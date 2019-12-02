d05fd8ba2f0a39e5fb2b2c486db7dac2d603db73
konouvang <konouvang@gmail.com>
2019-12-02 15:42:30 -0600

    getting ready to change content for tabs

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -13,15 +13,41 @@ export class SideDrawer {
     this.open = false;
   }
 
+  onContentChange(content: string) {
+    console.log(content);
+  }
+
   render() {
+    let mainContent = <slot />;
+    mainContent = (
+      <div id="contact-information">
+        <h2>Contact Information</h2>
+        <p>You can reach us via phone or email.</p>
+        <ul>
+          <li>Phone: 1234567890</li>
+          <li>E-mail: <a href="mailto:something@gmail.com">something@gmail.com</a></li>
+        </ul>
+      </div>
+    )
     return (
       <aside>
         <header>
           <h1>{this.title}</h1>
           <button onClick={this.onCloseDrawer.bind(this)}>X</button>
         </header>
+        <section id="tabs">
+          <button
+            class="active"
+            onClick={this.onContentChange.bind(this, 'nav')}
+            >
+              Navigation
+          </button>
+          <button onClick={this.onContentChange.bind(this, 'contact')}>
+            Contact
+          </button>
+        </section>
         <main>
-          <slot />
+          {mainContent}
         </main>
       </aside>
     );
