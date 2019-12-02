2163d57e07f0546d1420185507b01cc7cf1c7fc0
konouvang <konouvang@gmail.com>
2019-12-02 11:52:08 -0600

    build sidedrawer component

--- src/components.d.ts
+++ src/components.d.ts
@@ -10,53 +10,31 @@ import { HTMLStencilElement, JSXBase } from '@stencil/core/internal';
 
 
 export namespace Components {
-  interface MyComponent {
-    /**
-    * The first name
-    */
-    'first': string;
-    /**
-    * The last name
-    */
-    'last': string;
-    /**
-    * The middle name
-    */
-    'middle': string;
+  interface UcSideDrawer {
+    'title': string;
   }
 }
 
 declare global {
 
 
-  interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {}
-  var HTMLMyComponentElement: {
-    prototype: HTMLMyComponentElement;
-    new (): HTMLMyComponentElement;
+  interface HTMLUcSideDrawerElement extends Components.UcSideDrawer, HTMLStencilElement {}
+  var HTMLUcSideDrawerElement: {
+    prototype: HTMLUcSideDrawerElement;
+    new (): HTMLUcSideDrawerElement;
   };
   interface HTMLElementTagNameMap {
-    'my-component': HTMLMyComponentElement;
+    'uc-side-drawer': HTMLUcSideDrawerElement;
   }
 }
 
 declare namespace LocalJSX {
-  interface MyComponent {
-    /**
-    * The first name
-    */
-    'first'?: string;
-    /**
-    * The last name
-    */
-    'last'?: string;
-    /**
-    * The middle name
-    */
-    'middle'?: string;
+  interface UcSideDrawer {
+    'title'?: string;
   }
 
   interface IntrinsicElements {
-    'my-component': MyComponent;
+    'uc-side-drawer': UcSideDrawer;
   }
 }
 
@@ -66,7 +44,7 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
   export namespace JSX {
     interface IntrinsicElements {
-      'my-component': LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+      'uc-side-drawer': LocalJSX.UcSideDrawer & JSXBase.HTMLAttributes<HTMLUcSideDrawerElement>;
     }
   }
 }
